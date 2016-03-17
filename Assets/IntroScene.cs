82aec04ec401ba69274bbab4bb6ee2f31d582651
sebbdk <sebb@sebb.dk>
2016-03-17 23:33:22 +0100

    UI changes before launch
    Final changes, splash screen etc

---
+++ Assets/IntroScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("NextScene");
+	}
+
+	IEnumerator NextScene() {
+		yield return new WaitForSeconds(3);
+		Application.LoadLevel (Application.loadedLevel + 1);
+		yield return null;
+	}
+
+}
